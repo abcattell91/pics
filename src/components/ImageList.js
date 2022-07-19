1e97755d58b07a874d7d416f4e0ae7e4100074da
Alex Cattell <abcattell91@gmail.com>
2022-07-19 17:43:40 +0100

    rendered images in the browser with key props id for each image

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const ImageList = () => {
-  return <div>ImageList</div>
+const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return <img key={image.id } src={image.urls.regular}></img>
+  });
+
+  return <div>{images}</div>
 };
 
 export default ImageList;
